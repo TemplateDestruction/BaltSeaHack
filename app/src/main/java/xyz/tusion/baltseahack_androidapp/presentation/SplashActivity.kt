dfedcbfca8c317e56c606b985d7dc0ea06488dba
breakout <beefrollfresh@yandex.ru>
2019-11-10 13:01:48 +0300

    bottom nav act added

--- app/src/main/java/xyz/tusion/baltseahack_androidapp/presentation/SplashActivity.kt
+++ app/src/main/java/xyz/tusion/baltseahack_androidapp/presentation/SplashActivity.kt
@@ -6,13 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import xyz.tusion.baltseahack_androidapp.presentation.main.MainActivity
 import xyz.tusion.baltseahack_androidapp.presentation.main.MainActivity2
 import xyz.tusion.baltseahack_androidapp.presentation.screener.ScreenerActivity
+import xyz.tusion.baltseahack_androidapp.presentation.ui.BottomNavAct
 
 class SplashActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
 //        startActivity(Intent(this, ScreenerActivity::class.java))
-      startActivity(Intent(this, MainActivity2::class.java))
+    startActivity(Intent(this, BottomNavAct::class.java))
+//    startActivity(Intent(this, BottomNavAct::class.java))
+
+
 
 //      startActivity(Intent(this, CalendarActivity::class.java))
 
